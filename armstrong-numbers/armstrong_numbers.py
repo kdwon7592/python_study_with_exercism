f72730332a7c0f830bf4d20700e1aecd1fc3e1e1
김도원 <kdwon7592@naver.com>
2023-05-22 14:57:37 +0900

    [ADD] Exercism Python 2023.05.22

---
+++ armstrong-numbers/armstrong_numbers.py
@@ -0,0 +1,9 @@
+def is_armstrong_number(number):
+    digits = str(number)
+    num_digits = len(digits)
+    armstrong_sum = sum(int(digit) ** num_digits for digit in digits)
+
+    if armstrong_sum == number:
+        return True
+    else:
+        return False
